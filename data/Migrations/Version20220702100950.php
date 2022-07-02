1e281242d8cec6943db8574f771a2f379946fa91
Aurangzab Najam <aurangzaib_najam@ohotmail.com>
2022-07-02 15:20:00 +0500

    Creates migration file to define relationship

---
+++ data/Migrations/Version20220702100950.php
@@ -0,0 +1,29 @@
+<?php declare(strict_types=1);
+
+namespace Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220702100950 extends AbstractMigration
+{
+    public function getDescription()
+    {
+        return 'This migration is used to define foreign key constraints';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        $table = $schema->getTable('books');
+        $table->addForeignKeyConstraint('authors', ['author_id'], ['id'], [], 'author_foreign_key');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        $table = $schema->getTable('books');
+        $table->removeForeignKey('author_foreign_key');
+    }
+}
