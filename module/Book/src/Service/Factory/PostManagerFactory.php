93cec46a964bc5329cf30f92311fbfeb5e14a607
Aurangzab Najam <aurangzaib_najam@ohotmail.com>
2022-07-02 18:18:34 +0500

    Creates psot manager factory

---
+++ module/Book/src/Service/Factory/PostManagerFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Book\Service\Factory;
+
+use Interop\Container\ContainerInterface;
+use Zend\ServiceManager\Factory\FactoryInterface;
+use Album\Service\PostManager;
+
+/**
+ * This factory class is used to instantiate post manager
+ */
+class PostManagerFactory implements FactoryInterface
+{
+    public function __invoke(
+        ContainerInterface $container,
+        $requestedName,
+        array $options = null
+    ) {
+        $entityManager = $container->get('doctrine.entitymanager.orm_default');
+
+        return new PostManager($entityManager);
+    }
+}
