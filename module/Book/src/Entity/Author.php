d69a2ed2e9194f1a0963bf9d8e2c0aa40d4a9240
Aurangzab Najam <aurangzaib_najam@ohotmail.com>
2022-07-02 21:29:16 +0500

    Creates author entity

---
+++ module/Book/src/Entity/Author.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Book\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="authors")
+ */
+class Author
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(name="id")
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(name="name")
+     */
+    protected $name;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+}
