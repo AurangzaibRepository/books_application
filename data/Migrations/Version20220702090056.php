83a21310f767d5eb7a252ed404b179e8080f1d6c
Aurangzab Najam <aurangzaib_najam@ohotmail.com>
2022-07-02 15:08:50 +0500

    Creates authors migration

---
+++ data/Migrations/Version20220702090056.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types=1);
+
+namespace Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220702090056 extends AbstractMigration
+{
+    public function getDescription()
+    {
+        return 'This migration is used to generate authors table';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        $table = $schema->createTable('authors');
+        $table->addColumn('id', 'integer', ['notnull' => true]);
+        $table->addColumn('name', 'string', ['notnull' => true, 'length' => 80]);
+        $table->setPrimaryKey(['id']);
+    }
+
+    public function down(Schema $schema) : void
+    {
+        $schema->dropTable('authors');
+    }
+}
