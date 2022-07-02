d1aed7d5964a2fdf8efb66d6a5a87a941e693bb5
Aurangzab Najam <aurangzaib_najam@ohotmail.com>
2022-07-02 15:55:41 +0500

    Creates index controlleer

---
+++ module/Book/src/Controller/IndexController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Book\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class IndexController extends AbstractActionController
+{
+}
