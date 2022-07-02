4a5c4722e8f96f93b1d4ad290b1d3b8e28bc9eb2
Aurangzab Najam <aurangzaib_najam@ohotmail.com>
2022-07-02 18:09:16 +0500

    Creates postcontroller factory

---
+++ module/Book/src/Controller/Factory/PostControllerFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Album\Controller\Factory;
+
+use Interop\Container\ContainerInterface;
+use Zend\ServiceManager\Factory\FactoryInterface;
+use Book\Controller\PostController;
+use Book\Service\PostManager;
+
+class PostControllerFactory implements FactoryInterface
+{
+    public function __invoke(
+        ContainerInterface $container,
+        $requestedName,
+        array $options = null
+    ) {
+        $entityManager = $container->get('doctrine.entitymanager.orm_default');
+        $postManager = $container->get(PostManager::class);
+
+        return new PostController($entityManager, $postManager);
+    }
+}
