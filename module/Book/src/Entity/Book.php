f947a1c125c403b2a90baa605fb566ac87902554
Aurangzab Najam <aurangzaib_najam@ohotmail.com>
2022-07-02 16:05:26 +0500

    Creates book entity

---
+++ module/Book/src/Entity/Book.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Book\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * This class represents books table
+ * @ORM\Entity
+ * @ORM\Table(name="books")
+ */
+class Entity
+{
+
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(name="id")
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(name="title")
+     */
+    protected $title;
+
+    /**
+     * @ORM\Column(name="description")
+     */
+    protected $description;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+}
