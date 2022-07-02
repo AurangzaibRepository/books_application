f515a4bfde3f6f85351f358bc344615a2e5e01f3
Aurangzab Najam <aurangzaib_najam@ohotmail.com>
2022-07-02 18:43:34 +0500

    Creates post controler

---
+++ module/Book/src/Controller/PostController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Book\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+use Book\Form\PostForm;
+
+class PostController extends AbstractActionController
+{
+
+    /**
+     * @var Doctrine\ORM\EntityManager
+     */
+    private $entityManager;
+
+    /**
+     * @var Book\Service\PostManager
+     */
+    private $postManager;
+
+    public function __construct(
+        $entityManager,
+        $postManager
+    ) {
+        $this->entityManager = $entityManager;
+        $this->postManager = $postManager;
+    }
+}
