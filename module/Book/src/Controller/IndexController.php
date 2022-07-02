29b3f563ab2201a913b499710a431fb00da2c017
Aurangzab Najam <aurangzaib_najam@ohotmail.com>
2022-07-02 16:09:48 +0500

    Injects entity amanger

--- module/Book/src/Controller/IndexController.php
+++ module/Book/src/Controller/IndexController.php
@@ -4,7 +4,22 @@ namespace Book\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
+use Book\Entity\Book;
 
 class IndexController extends AbstractActionController
 {
+    /**
+     * @var Doctrine\ORM\EntityManager
+     */
+    protected $entityManager;
+
+    public function __construct($entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    public function indexAction()
+    {
+        return new ViewModel();
+    }
 }
