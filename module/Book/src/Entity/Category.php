ddd2bafeb17ebbc2043e86b2d97974de6dfbc6ff
Aurangzaib Najam <aurangzaib_najam@ohotmail.com>
2022-07-03 13:15:46 +0500

    Creates categry enoyry

---
+++ module/Book/src/Entity/Category.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Book\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="categories")
+ */
+class Category
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(name="id")
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(name="name")
+     */
+    protected $name;
+}
